01f21fe116dc4daab6ce55b558104427a640180d
Bkfreedom <88872561+Blake556@users.noreply.github.com>
2023-04-13 10:51:22 -0700

    Restyled cart page

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -17,34 +17,66 @@ let cart = props.cartItems
 
     return(
      
-        // <div className='cart-body'>
-        //     {/* { cartItems.length === 0 && <div>Cart is Empty</div> } */}
-        // </div>
-      <div>
-          <h1>Cart page</h1>
-          <div className='cart-body'>
-          { props.cartItems.length === 0 && <div> No items in cart</div> }
-          </div>
+        
+      <div className='cart-body'>
+          <h1 className='cart-header'>Checkout</h1>
+          <div className='container col-6 cart-container' >
+          {/*{ props.cartItems.length === 0 && <div> No items in cart</div> }
+         
        
       
-          <div className='cart-body'>
+         
         {    cart.map(item => {
           return (
-              <div key={item.id} className='cart-container' >
-                <img src={item.image}/>
-                <h2>{item.name}</h2>
-                <h4>{item.price}</h4>
+              <div key={item.id} className='container col-8 cart-container' >
+             
+                <div className='row'>
+                  <div className='col col-3'>
+                    <img src={item.image} width='150px' />
+                  </div>
+                  <div className='col col-6'>
+                    <h2>{item.name}</h2>
+                    <div className='flex-box'>
+                      <button className='btn btn-danger'>-</button>
+                      <input className="quanity-option form-control "  type='number'></input>
+                    </div>
+                    <button className='btn btn-success'>+</button>
+                  </div>
+                  <div className='col col-3'>
+                  <h4>{item.price}</h4>
+                  </div>
+                </div>
               </div>
           )
-        })}
+        })} */}
+
+       
+             
+                <div className='row cart-item-container'>
+                  <div className='col col-3 justify-content-start img-col'>
+                    <img className='cart-item-img  d-flex justify-content-start' src={johnWhick} width='150px' />
+                  </div>
+                  <div className='col col-8 item-details-qunaity'>
+                    <div className='d-flex align-items-start flex-column'>
+                      <h4 className=''>John Whick</h4>
+                      <h6>Canvas</h6>
+                    </div>
+                    <div className='d-flex'>
+                      <button className='btn btn-danger quanity-btn'>-</button>
+                      <input className="quanity-option form-control " maxlength="2" type='tel'></input>
+                      <button className='btn btn-success quanity-btn'>+</button>
+                    </div>
+                  </div>
+                  <div className='col col-1 d-flex  align-items-end justify-content-end  '>
+                    <h4 className='item-price'>$100</h4>
+                  </div>
+                </div>
+                
+        </div>
            
-                    {/* <div className='cart-container'>
-                      <img src={johnWhick} width="200px"></img>
-                      <span>John Whick canvas</span>
-                      
-                    </div> */}
-          </div>
-       </div>
+                   
+         
+        </div>
          
 
     )

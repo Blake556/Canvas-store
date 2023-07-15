5eab100674f0c0e1bc0b3a78bcd37f424f299171
Bkfreedom <88872561+Blake556@users.noreply.github.com>
2023-07-15 11:14:32 -0700

    image gets bigger when hovered over

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,47 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/product.css";
-//import storeProducts from '../productData'
-import { useState } from "react";
 
 function Product(props) {
   const { id, image, name, descrip, price } = props;
 
-  const [addItem, setItem] = useState({
-    id: id,
-    key: id,
-    image: image,
-    name: name,
-    descrip: descrip,
-    price: price,
-    qty: 1,
-  });
+  // Add state to keep track of hover state
+  const [isHovered, setIsHovered] = useState(false);
 
   function addToCart() {
-    props.continueToPass(addItem);
-    setItem({
+    props.continueToPass({
       id: id,
       key: id,
       image: image,
       name: name,
       descrip: descrip,
       price: price,
+      qty: 1,
     });
-    //event.preventDefault()
-    //console.log(addItem)
   }
 
-
-
   return (
-    <div className="product-container animation">
-    <div className='product-img-container'>
-      {<img className="product-details product-img animation" src={image} />}
-    </div>
-      {<h3 className="product-details product-name">{name}</h3>}
-      {<h5 className='product-details product-description'>{descrip}</h5>}
-      {<h3 className="product-details product-price">{'$' + price}</h3>}
-      <button onClick={addToCart} className="btn btn-secondary product-details addBtn">
-        Buy 
+    <div className="product-container">
+      <div
+        className="product-img-container"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img
+          className="product-details product-img"
+          src={image}
+          alt={name}
+          style={{
+            transform: isHovered ? "scale(1.1)" : "scale(1)",
+          }}
+        />
+      </div>
+      <h3 className="product-details product-name">{name}</h3>
+      <h5 className="product-details product-description">{descrip}</h5>
+      <h3 className="product-details product-price">{"$" + price}</h3>
+      <button
+        onClick={addToCart}
+        className="btn btn-secondary product-details addBtn"
+      >
+        Buy
       </button>
     </div>
   );

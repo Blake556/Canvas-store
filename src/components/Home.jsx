b92d49114d1d3a693bc86f0e5765cbff8904298d
Bkfreedom <88872561+Blake556@users.noreply.github.com>
2023-08-27 12:13:18 -0700

    Revert "Updates"
    This reverts commit 5bc02243c25672b2d46add9ef493427e7637a493.

---
+++ src/components/Home.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import '../styles/home.css';
+import Product from './Product'
+
+
+function Home(props) {
+    // const [itemSelected, setSelected] = useState({
+    //     image: '',
+    //     name: '',
+    //     price: ''
+    // })
+
+    const { data } = props;
+
+    // function handleClick() {
+    //     //setSelected(console.log())
+    //     //props.onAdd(props)
+       
+        
+    //     //console.log()
+    //     //event.preventDefault()
+    // }
+
+    function continueToPass(addItem) {
+        props.onAdd(addItem) 
+    }
+
+    return (
+        <div className='body'>
+            <h1 className='header'>Canvas store</h1>
+                <div className='main-content'>
+                { data.map((item, index) => {
+                    return (
+                        <Product 
+                            key={item.id}
+                            id={item.id}
+                            image={item.image}
+                            name={item.name}
+                            descrip={item.descrip}
+                            price={item.price}
+                            continueToPass={continueToPass}
+                            //onAdd={handleClick}
+                        />
+                    )
+                }) }
+                </div>
+        </div>
+    );
+    
+}
+
+export default Home;
+
+
+
+// {
+//     image: 
+//     name: 
+//     price: 
+// }
---
+++ src/components/Product.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "../styles/product.css";
+
+function Product(props) {
+  const { id, image, name, descrip, price } = props;
+
+  // Add state to keep track of hover state
+  const [isHovered, setIsHovered] = useState(false);
+
+  function addToCart() {
+    props.continueToPass({
+      id: id,
+      key: id,
+      image: image,
+      name: name,
+      descrip: descrip,
+      price: price,
+      qty: 1,
+    });
+  }
+
+  return (
+    <div className="product-container">
+      <div
+        className="product-img-container"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <img
+          className="product-details product-img"
+          src={image}
+          alt={name}
+          style={{
+            transform: isHovered ? "scale(1.1)" : "scale(1)",
+          }}
+        />
+      </div>
+      <h3 className="product-details product-name">{name}</h3>
+      <h5 className="product-details product-description">{descrip}</h5>
+      <h3 className="product-details product-price">{"$" + price}</h3>
+      <button
+        onClick={addToCart}
+        className="btn btn-secondary product-details addBtn"
+      >
+        Buy
+      </button>
+    </div>
+  );
+}
+
+export default Product;
